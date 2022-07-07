dd3f4b79174fd52b5de0265118e9f6148ffa132c
bamba2477 <94611436+bamba2477@users.noreply.github.com>
2022-07-07 19:53:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 video = "";
+Objects = [];
 function preload(){
     video = createVideo('video.mp4');
     video.hide();
@@ -8,7 +9,22 @@ function setup(){
     canvas.center();
 }
 function draw(){
-    image ( video,0,0,480,380)
+    image ( video,0,0,480,380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult); 
+        for (i = 0; Objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "status: obgects detecting";
+            document.getElementById("number_of_obgects").innerHTML = "number of detected obgects are :"+Objects.length;
+            fill("#00ffdd");
+            percent = floor(Objects[i].confidence * 100);
+            text(Objects[i].label+" "+percent+"%",Objects[i].x, Objects[i].y +20);
+            noFill();
+            stroke("#ff8c00");
+            rect(Objects[i].x , Objects[i].y ,  Objects[i].width , Objects[i].height);
+        }
+    }
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd',modeloaded);
@@ -20,4 +36,11 @@ function modeloaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult (error,results) {
+    if(error) {
+console.log(error);
+    }
+    console.log(results);
+    Objects = results;
 }
